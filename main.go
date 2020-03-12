594d64b60807ed0f6fe5ca21f0cd091d39d44002
May Marcus (U119240) <marcus.may@mobi.ch>
2020-03-12 21:01:28 +0100

    improve readability

--- main.go
+++ main.go
@@ -20,19 +20,10 @@ func main() {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		var id uint64
 		err := db.Update(func(tx *bolt.Tx) error {
-			bucket := tx.Bucket([]byte("MyBucket"))
-			if bucket == nil {
-				bucket, err = tx.CreateBucket([]byte("MyBucket"))
-			}
+			bucket, err := openBucket(tx)
 
-			value := bucket.Get([]byte("a"))
-			if value == nil {
-				value = toBytes(1)
-				bucket.Put([]byte("a"), value)
-			}
-
-			id = toInt(value)
-			err := bucket.Put([]byte("a"), toBytes(id+1))
+			id = getOrDefault(bucket, 1)
+			err = bucket.Put([]byte("a"), toBytes(id+1))
 
 			return err
 		})
@@ -44,6 +35,23 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func openBucket(tx *bolt.Tx) (*bolt.Bucket, error) {
+	bucket := tx.Bucket([]byte("MyBucket"))
+	if bucket == nil {
+		return tx.CreateBucket([]byte("MyBucket"))
+	}
+	return bucket, nil
+}
+
+func getOrDefault(bucket *bolt.Bucket, def uint64) uint64 {
+	value := bucket.Get([]byte("a"))
+	if value == nil {
+		value = toBytes(def)
+		bucket.Put([]byte("a"), value)
+	}
+	return toInt(value)
 }
 
 func toInt(value []byte) uint64 {
